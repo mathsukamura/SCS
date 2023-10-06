e45120cd766accea4c2d098aa0d7381e880f9b3f
mathsukamura <matheus.yuri.melo@gmail.com>
2023-10-06 02:03:22 -0300

    push project

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3, // Quantos slides visíveis por vez
+    spaceBetween: 0, // Espaçamento entre os slides
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
